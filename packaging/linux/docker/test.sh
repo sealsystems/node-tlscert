d779c90316b342fd02c39c96c36dfa6255a9f937
comgit <com.git@sealsystems.de>
2022-12-29 20:36:50 +0100

    Update build configuration

--- packaging/linux/docker/test.sh
+++ packaging/linux/docker/test.sh
@@ -3,6 +3,13 @@
 PACKAGE_NAME=$(jq -r .name < package.json)
 PACKAGE_VERSION=$(jq -r .version < package.json)
 
+echo "Pulling container image..."
+
+while ! docker pull ghcr.io/sealsystems/${PACKAGE_NAME}:${PACKAGE_VERSION}.${BUILD_NUMBER}; do
+  echo "Retrying in 10 seconds"
+  sleep 10
+done
+
 echo "Testing container image..."
 
 # Patch Dockerfile
