291fdece9da765e98f40edb5556547e505ce5a0d
comgit <com.git@sealsystems.de>
2024-02-19 20:36:12 +0100

    Update build configuration

--- packaging/windows/msi/build.patch-images.ps1
+++ packaging/windows/msi/build.patch-images.ps1
@@ -1,22 +1,34 @@
 $ErrorActionPreference = "Stop"
-Set-PSDebug -Trace 1
+# Set-PSDebug -Trace 1
 
-function PatchImages($Product, $Version, $Description) {
-  Write-Host "PatchImages called with product $Product, version $Version and description $Description"
+function CopyFile($Path, $Destination) {
+  Write-Host "Copying file from: ""$Path"", to: ""$Destination""..."
+  Copy-Item -Path "$Path" -Destination "$Destination" -Force -ErrorAction SilentlyContinue
+  if (-Not $?) {
+    Write-Host $Error[0].ToString()
+  }
+}
+
+function PatchImages($Product, $Version, $ProductName) {
+  Write-Host "PatchImages called with product $Product, version $Version and product name $ProductName"
   $Repo = "seal-icons"
   $TargetResourceDir = "packaging/windows/msi/resource/images"
   # check for product or service
   Write-Host -NoNewline "Patching images for "
-  if ("$Description" -ne "") {
-    Write-Host -NoNewline "product "
+  if ("$ProductName" -ne "") {
+    Write-Host -NoNewline "bundle $ProductName"
   } else {
-    Write-Host -NoNewline "service "
+    Write-Host -NoNewline "service $Product"
     # for services use platform icons
-    $Product = "SEAL"
+    $Product = "service"
   }
   switch ($Product) {
     {($_ -eq "plossys") -Or ($_ -eq "out-ngn")} {
       $name = "PLOSSYS_Output_Engine"
+      Break
+    }
+    "service" {
+      $name = "SEAL"
       Break
     }
     "build-pipeline-playground" {
@@ -27,18 +39,17 @@ function PatchImages($Product, $Version, $Description) {
       $name = "Default"
     }
   }
-  Write-Host "$Product from $Repo/$name"
+  Write-Host " from $Repo/$name (ignoring version $Version)."
   # if ($Version -ne "") {
   #   $branch = "--branch $Version"
   #   Write-Host "Using branch info: Version = $Version"
   # }
-  Write-Host "...ignoring version $Version..."
-
+  
   # Clone images from github
   git clone $branch https://${env:GITHUB_TOKEN}@github.com/sealsystems/$Repo
   $Dir = "seal-icons/$name"
   if (-Not (Test-Path -Path $Dir)) {
-    Write-Host "Image directory $Dir not found!"
+    Write-Host "Image directory $Dir not found! Images are not patched. "
     return 13
   }
   Write-Host Listing of $Dir
@@ -48,48 +59,31 @@ function PatchImages($Product, $Version, $Description) {
   Write-Host Listing of current $TargetResourceDir
   ls $TargetResourceDir
 
-  # copy Icon
+  # always copy Icon
   $File = (Get-ChildItem -Path "$Dir" *.ico).Name
-  $Path = "$Dir/$File"
-  $Destination = "$TargetResourceDir/logo.ico"
-  Copy-Item -Path "$Path" -Destination "$Destination" -Force -ErrorAction SilentlyContinue
-  if (-Not $?) {
-    WriteHost $Error[0].ToString()
-    WriteHost "From: ""$Path"", to: ""$Destination"""
+  if ($File) {
+    CopyFile "$Dir/$File" "$TargetResourceDir/logo.ico" 
   }
   # copy logo for bundle banner
-  if ("$Description" -ne "") {
+  if ("$ProductName" -ne "") {
     $File = (Get-ChildItem -Path "$Dir" *.png).Name
-    if (-Not $?) {
-      WriteHost $Error[0].ToString()
-      WriteHost "No PNG file for bundle banner found. Using copy of icon file!"
+    if (-Not $File) {
+      Write-Host "No PNG file for bundle banner found. Trying copy of icon file!"
       $File = (Get-ChildItem -Path "$Dir" *.ico).Name
     }
-    $Path = "$Dir/$File"
-    $Destination = "$TargetResourceDir/logo.png"
-    Copy-Item -Path "$Path" -Destination "$Destination" -Force -ErrorAction SilentlyContinue
-    if (-Not $?) {
-      WriteHost $Error[0].ToString()
-      WriteHost "From: ""$Path"", to: ""$Destination"""
+    if ($File) {
+      CopyFile "$Dir/$File" "$TargetResourceDir/logo.png" 
     }
   } else {
     # copy banner
     $File = Get-ChildItem -Path "$Path" | Where-Object {$_.Name -match '.*Banner.*\.bmp'}
-    $Path = "$Dir/$File"
-    $Destination = "$TargetResourceDir/msi_banner.bmp"
-    Copy-Item -Path "$Path" -Destination "$Destination" -Force -ErrorAction SilentlyContinue
-    if (-Not $?) {
-      WriteHost $Error[0].ToString()
-      WriteHost "From: ""$Path"", to: ""$Destination"""
+    if ($File) {
+      CopyFile "$Dir/$File" "$TargetResourceDir/msi_banner.bmp" 
     }
     # copy dialog
     $File = Get-ChildItem -Path "$Path" | Where-Object {$_.Name -match '.*Dialog.*\.bmp'}
-    $Path = "$Dir/$File"
-    $Destination = "$TargetResourceDir/msi_dialog.bmp"
-    Copy-Item -Path "$Path" -Destination "$Destination" -Force -ErrorAction SilentlyContinue
-    if (-Not $?) {
-      WriteHost $Error[0].ToString()
-      WriteHost "From: ""$Path"", to: ""$Destination"""
+    if ($File) {
+      CopyFile "$Dir/$File" "$TargetResourceDir/msi_dialog.bmp" 
     }
   }
   Remove-Item -Recurse -Force seal-icons
@@ -98,4 +92,4 @@ function PatchImages($Product, $Version, $Description) {
 }
 
 # Patch images
-PatchImages "$env:PACKAGE_NAME" "$env:PACKAGE_VERSION" "$env:PRODUCT_DESCRIPTION" 
+PatchImages "$env:PACKAGE_NAME" "$env:PACKAGE_VERSION" "$env:PRODUCT_NAME" 
